1aee4308823c8258ff5bab643bddae742fd41231
Kandinor <andinokarla447@gmail.com>
2024-05-08 23:56:21 +0200

    corregido ejercicio cadenas y agregado el 2

--- EJERCICIOS CLASE/formularios/E_07_Cadenas/Cadenas_01_palindromoYdemas/index.php
+++ EJERCICIOS CLASE/formularios/E_07_Cadenas/Cadenas_01_palindromoYdemas/index.php
@@ -1,50 +1,63 @@
 <?php
-//variables
-$cadena ='';
+// Variables
+$cadena = '';
 $errores = [];
+$resultados = [];
 
-if(isset($_GET['enviar'])){
-
-    if(isset($_GET['cadena']) && $_GET['cadena'] != ""){
-        $cadena = $_GET['cadena'];
-    }else{
-        $errores[] = "*Debe ingresar una cadena";
-    }
-
-    if(empty($errores)){
-        $cadena = strtolower($cadena);
-        $cadena = str_replace(" ", "", $cadena);
-        
-        if($cadena == strrev($cadena)){
-            echo "La cadena es palindromo";
-        }else{
-            echo "La cadena no es palindromo";
-        }
-
-        
-
-    }
+function contarVocales($cadena) {
+    return preg_match_all('/[aeiouáéíóú]/i', $cadena);
 }
 
+function contarConsonantes($cadena) {
+    return preg_match_all('/[bcdfghjklmnpqrstvwxyz]/i', $cadena);
+}
 
+function esPalindromo($cadena) {
+    $limpio = str_replace(' ', '', strtolower($cadena));
+    return $limpio === strrev($limpio) ? 'sí' : 'no';
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['cadena'])) {
+    if (trim($_GET['cadena']) !== "") {
+        $cadena = $_GET['cadena'];
+        $resultados['vocales'] = contarVocales($cadena);
+        $resultados['consonantes'] = contarConsonantes($cadena);
+        $resultados['palindromo'] = esPalindromo($cadena);
+    } else {
+        $errores[] = "*Debe ingresar una cadena.";
+    }
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>manejo de cadena Palindromo</title>
+    <title>Análisis de Cadena</title>
 </head>
 <body>
     <form action="" method="get">
-        <label for="">Ingrese una cadena</label>
-        <input type="text" name="cadena" value="">
-
+        <label for="cadena">Ingrese una cadena:</label>
+        <input type="text" name="cadena" id="cadena" value="<?php echo htmlspecialchars($cadena); ?>">
         <input type="submit" value="Enviar">
     </form>
 
-    <div class="resultado">
+    <?php if (!empty($errores)): ?>
+        <div class="errores">
+            <?php foreach ($errores as $error): ?>
+                <p style="color: red;"><?php echo $error; ?></p>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 
-    </div>
+    <?php if (!empty($resultados)): ?>
+        <div class="resultado">
+            <ul>
+                <li>Número de vocales: <?php echo $resultados['vocales']; ?></li>
+                <li>Número de consonantes: <?php echo $resultados['consonantes']; ?></li>
+                <li>Palíndromo: <?php echo $resultados['palindromo']; ?></li>
+            </ul>
+        </div>
+    <?php endif; ?>
 </body>
 </html>
